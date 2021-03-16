2e9c68ce7e54af5e795fbbca5e31c36fa90084b4
chenyueban <jasonchan0527@gmail.com>
2021-03-16 20:34:38 +0800

    docs: build docs based dumi

---
+++ docs/src/.umi/core/history.ts
@@ -0,0 +1,26 @@
+// @ts-nocheck
+import {
+  createBrowserHistory,
+  History,
+} from '/Users/chenyueban/Documents/works/neoi/node_modules/.pnpm/@umijs/runtime@3.4.2_react@16.14.0/node_modules/@umijs/runtime'
+
+let options = {
+  basename: '/docs/',
+}
+if ((<any>window).routerBase) {
+  options.basename = (<any>window).routerBase
+}
+
+// remove initial history because of ssr
+let history: History = process.env.__IS_SERVER
+  ? null
+  : createBrowserHistory(options)
+export const createHistory = (hotReload = false) => {
+  if (!hotReload) {
+    history = createBrowserHistory(options)
+  }
+
+  return history
+}
+
+export { history }
